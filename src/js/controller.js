1b14bb078d046ae0a72d8ce6de34c1ed8338c3bb
SaiAvinash2002 <saiavinashvenkata@gmail.com>
2025-05-20 16:29:05 -0500

    forkify

---
+++ src/js/controller.js
@@ -0,0 +1,56 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// NEW API URL (instead of the one shown in the video)
+// https://forkify-api.jonas.io
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    // The fetch will creates a promise and then when we give await the code will wait till the promise is accepted or rejected. But this doesn't interrupts the main thread execution.
+
+    if (!id) return;
+
+    // Load Spinner
+    recipeView.renderSpinner(recipeContainer);
+
+    // Loading Recipe
+    await model.loadRecipe(id);
+
+    // Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    await model.loadSearchResults('pizza');
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResults();
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
+// Instead of writing this we can use the above one
+// window.addEventListener('hashchange', showRecipe);
---
+++ src/js/model.js
@@ -0,0 +1,52 @@
+import { API_URL } from './config.js';
+import { getJSON } from './helper.js';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+
+    const { recipe } = data.data;
+    // console.log(res, data);
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    // console.error(`${err} 💥💥💥💥`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(recipe => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        image: recipe.image_url,
+      };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+loadSearchResults('pizza');
